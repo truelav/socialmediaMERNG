f38c2fa85ac97eecb269d8b3ea5d2b447ee4efba
truelav <madan.serghei@yahoo.com>
2022-05-12 15:34:20 -0700

    implementing plain react front end pages registration / login / home

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+// import { Container } from 'semantic-ui-react';
 
+// import './semanticUI/semantic.min.css'
+import './App.css'
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
-    <div className="App">
-      This is the app
-    </div>
+
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={ <Home/> } />
+          <Route exact path="/login" element={ <Login/> } />
+          <Route exact path="/register" element={ <Register/> } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
